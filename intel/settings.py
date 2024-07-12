8b8bcda9ea2bab5e1239c4121f9976d906a6631e
mikegyamfi <codeage20@gmail.com>
2024-07-12 07:16:27 +0000

    allowed hosts

--- intel/settings.py
+++ intel/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'amazing-data-hub-8e677.ondigitalocean.app',
+    'sedatahub-f21ac064b74a.herokuapp.com',
     'www.amazingdatahubgh.com'
 ]
 
